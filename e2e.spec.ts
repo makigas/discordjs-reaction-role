6178ee413d7a1e10a507e1945e337d286af11a58
Dani Rodríguez <dani@danirod.es>
2021-08-14 11:00:15 +0200

    feat: add E2E test to live check the lib works
    This is intended to be used with ts-node in development mode. It imports
    the library and listens for a reaction on a specific message to add a
    specific bot.
    
    This code should be used to assert that the bot works. Provide the
    token, the message ID and the role ID using environment variables, such
    as
    
    * BOT_TOKEN: for the login token
    * ROLE: for the snowflake of the Role I should add
    * MESSAGE: for the snowflake of the Message I should listen

---
+++ e2e.spec.ts
@@ -0,0 +1,48 @@
+/*
+ * This is a functional test to assert that the library behaves as
+ * it should. Yes, you should write unit tests, but your unit test
+ * will not talk to the real Discord. Run this script with ts-node
+ * and let the bot play to assert that it is working as expected.
+ * Also, you can use this file to learn how to use the library.
+ */
+
+import { Client, Intents } from "discord.js";
+import ReactionRole from "./index";
+
+if (!process.env.BOT_TOKEN || !process.env.ROLE || !process.env.MESSAGE) {
+  console.error("Missing config");
+  process.exit(1);
+}
+const token: string = process.env.BOT_TOKEN;
+const role: string = process.env.ROLE;
+const message: string = process.env.MESSAGE;
+
+const client = new Client({
+  partials: ["MESSAGE", "REACTION"],
+  ws: {
+    intents:
+      Intents.FLAGS.GUILD_MESSAGE_REACTIONS |
+      Intents.FLAGS.GUILD_MESSAGES |
+      Intents.FLAGS.GUILDS,
+  },
+});
+
+new ReactionRole(client, [
+  {
+    messageId: message,
+    reaction: "✅",
+    roleId: role,
+  },
+]);
+
+client.on("ready", () => {
+  console.log("Bot is online");
+});
+
+client.login(token);
+process.on("SIGINT", () => {
+  client.destroy();
+});
+process.on("SIGTERM", () => {
+  client.destroy();
+});
