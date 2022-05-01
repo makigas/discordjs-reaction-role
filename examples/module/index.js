2d4fbe5e6399b8cb29c7d801cf0270ae88c70af3
Dani Rodríguez <dani@danirod.es>
2022-05-01 12:00:55 +0200

    chore: add examples in JavaScript

---
+++ examples/module/index.js
@@ -0,0 +1,47 @@
+/*
+ * This is an integration example that shows how to use this package
+ * in JavaScript when the project is making use of JavaScript modules
+ * (so you import using "import from").
+ */
+
+import { Client, Intents } from "discord.js";
+import { ReactionRole } from "discordjs-reaction-role";
+
+// Make sure that all the three environment variables are declared.
+["BOT_TOKEN", "ROLE", "MESSAGE"].forEach((env) => {
+  if (!process.env[env]) {
+    console.error(`Missing environment variable: ${env}`);
+    process.exit(1);
+  }
+});
+
+// Create a client with the intents and partials required.
+const client = new Client({
+  partials: ["MESSAGE", "REACTION"],
+  intents:
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS |
+    Intents.FLAGS.GUILD_MESSAGES |
+    Intents.FLAGS.GUILDS,
+});
+
+// Create a new manager and use it.
+const configuration = [
+  {
+    messageId: process.env.MESSAGE,
+    reaction: "✅", // :white_check_mark:
+    roleId: process.env.ROLE,
+  },
+];
+const manager = new ReactionRole(client, configuration);
+
+// Start the bot.
+client.on("ready", () => console.log("Bot is online!"));
+client.login(process.env.BOT_TOKEN);
+
+// Stop the bot when the process is closed (via Ctrl-C).
+const destroy = () => {
+  manager.teardown();
+  client.destroy();
+};
+process.on("SIGINT", destroy);
+process.on("SIGTERM", destroy);
